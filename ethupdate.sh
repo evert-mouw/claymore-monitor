3a1e7234a8ec9631a2b4993892ebf76107656e58
Evert Mouw <post@evert.net>
2017-12-19 22:44:15 +0100

    easier to change personal settings

--- ethupdate.sh
+++ ethupdate.sh
@@ -4,12 +4,14 @@
 # use with cron (cronie) with
 # ln -s THISFILE /etc/cron.daily/ethstats
 # Evert Mouw <post@evert.net>
-# 2017-12-10
+# 2017-12-10, 2017-12-19
 
-DWARF=$(curl -s "http://dwarfpool.com/eth/api?wallet=0x20843145b36b5e7415c4243ee4cd23aea4df750d&email=post@evert.net")
+EMAIL="yourname@domain.tld"
+WALLET="0x............"
+
+DWARF=$(curl -s "http://dwarfpool.com/eth/api?wallet=$WALLET&email=$EMAIL")
 
 ETHEUR="https://www.coingecko.com/en/price_charts/ethereum/eur"
-BALANCE="https://etherchain.org/account/0x20843145b36b5e7415c4243ee4cd23aea4df750d"
+BALANCE="https://etherchain.org/account/$WALLET"
 
-echo -e "$DWARF\n\n$ETHEUR\n\n$BALANCE" | mail -s "#~ dwarfpool" post@evert.net
-
+echo -e "$DWARF\n\n$ETHEUR\n\n$BALANCE" | mail -s "#~ dwarfpool" $EMAIL
